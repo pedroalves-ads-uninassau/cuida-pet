ac7527b16869432b7c2e8a16321c91b5fd75d93d
Pedro Alves <01864946@sempreuninassau.com.br>
2025-12-01 15:42:39 +0000

    Favicon manual

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,35 @@
-import "./globals.css";
-import { Baloo_2 } from "next/font/google";
+  import "./globals.css";
+  import { Baloo_2 } from "next/font/google";
 
-const baloo = Baloo_2({ subsets: ["latin"], weight: ["400", "600", "700"] });
+  const baloo = Baloo_2({ subsets: ["latin"], weight: ["400", "600", "700"] });
 
-export const metadata = {
-  title: "Cuida Pet - Conectando Pessoas e Pets com Amor e Tecnologia",
-  description: "Cuida Pet é a plataforma perfeita para cuidar, agendar e se conectar com quem também ama os animais.",
-  icons: {
-    icon: '/icon.png',
-  },
-};
+  export const metadata = {
+    title: "Cuida Pet - Conectando Pessoas e Pets com Amor e Tecnologia",
+    description: "Cuida Pet é a plataforma perfeita para cuidar, agendar e se conectar com quem também ama os animais.",
+    icons: {
+      icon: '/icon.png',
+    },
+  };
 
-import { AppProvider } from "@/context/AppContext";
+  import { AppProvider } from "@/context/AppContext";
 
 
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="pt-BR">
-      <body className={`${baloo.className} bg-gray-50 flex flex-col min-h-screen`}>
-        <AppProvider>
-          {children}
-        </AppProvider>
-      </body>
-    </html>
-  );
-}
+  export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <html lang="pt-BR">
+        <head>
+          <link rel="icon" href="/icon.png" sizes="any" />
+        </head>
+        <body className={`${baloo.className} bg-gray-50 flex flex-col min-h-screen`}>
+          <AppProvider>
+            {children}
+          </AppProvider>
+        </body>
+      </html>
+    );
+  }
